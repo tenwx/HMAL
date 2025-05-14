a70b169d5cd00631cd5833275de9a0cd1193c0bc
tenwx <tenwx@foxmail.com>
2025-05-14 08:54:09 +0000

    modified to x.y.z

---
+++ app/src/main/java/x/y/z/util/SuUtils.kt
@@ -0,0 +1,10 @@
+package x.y.z.util
+
+import com.topjohnwu.superuser.Shell
+
+object SuUtils {
+
+    fun execPrivileged(cmd: String): Boolean {
+        return Shell.cmd(cmd).exec().isSuccess && Shell.isAppGrantedRoot() == true
+    }
+}
